76ade8eaca3ca03c4dc2297b01f2d0fb15444327
Andrejev Denis <andrejevdenis00@gmail.com>
2024-12-14 12:05:04 +0400

    1 Draft

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+import time
+
+import pytest
+from selene import Browser, Config
+from selenium import webdriver
+import allure
+from selenium.webdriver.chrome.options import Options
+from utils import attach
+
+
+@pytest.fixture(scope='function')
+def browser_management():
+
+    with allure.step("Open source page"):
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "126.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options
+        )
+        browser = Browser(Config(driver))
+
+        yield
+
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
+        attach.add_video(browser)
+
+        browser.quit()
